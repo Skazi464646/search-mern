0ee73fe4df648a37d27d8d98cac97e7393c77fac
skazi464646 <shantanu.bhosale0798@gmail.com>
2025-10-21 00:40:56 +0400

    And I can make it visible in mobile view as well.

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -61,14 +61,6 @@ const Header: React.FC<HeaderProps> = ({ onCategorySelect }) => {
           <div className={styles.navItem}>
             <div className={styles.vkButton}>VK</div>
           </div>
-          
-          <div className={styles.navItem}>
-            <div className={styles.gridIcon}>
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M4 4h4v4H4V4zm6 0h4v4h-4V4zm6 0h4v4h-4V4zM4 10h4v4H4v-4zm6 0h4v4h-4v-4zm6 0h4v4h-4v-4zM4 16h4v4H4v-4zm6 0h4v4h-4v-4zm6 0h4v4h-4v-4z"/>
-              </svg>
-            </div>
-          </div>
         </nav>
       </header>
 
